b83faf23b199199c7c625dd9fe9e5eded7fcbaac
Ignatius A A Saragih <ignatius.a.a.s@gmail.com>
2024-11-13 15:24:27 +0700

    Initial commit

---
+++ BE/lokakarya/src/main/java/co/id/ogya/lokakarya/LokakaryaApplication.java
@@ -0,0 +1,13 @@
+package co.id.ogya.lokakarya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LokakaryaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LokakaryaApplication.class, args);
+	}
+
+}
---
+++ BE/lokakarya/src/test/java/co/id/ogya/lokakarya/LokakaryaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.ogya.lokakarya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LokakaryaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
