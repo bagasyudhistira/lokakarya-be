04bac4435201519da04bf5512ddf3503d27c1e28
Ignatius A A Saragih <ignatius.a.a.s@gmail.com>
2024-11-14 12:04:02 +0700

    Something I forgot

--- lokakarya/src/main/java/co/id/ogya/lokakarya/config/OpenApiConfig.java
+++ lokakarya/src/main/java/co/id/ogya/lokakarya/config/OpenApiConfig.java
@@ -1,4 +1,44 @@
 package co.id.ogya.lokakarya.config;
 
-public class OpenApiConfig {
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class OpenApiConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("/swagger-ui.html");
+        registry.addViewController("/swagger-ui").setViewName("/swagger-ui.html");
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info()
+                        .title("Lokakarya")
+                        .description("Lokakarya Kel 1")
+                        .version("0.1")
+//                        .termsOfService("no")
+//                        .license(new License()
+//                                .name("Apache License Version 2.0")
+//                                .url("https://www.apache.org/licenses/LICENSE-2.0"))
+//                        .contact(new Contact()
+//                                .name("")
+//                                .email(""))
+                );
+    }
+
+    @Bean
+    GroupedOpenApi allApis() {
+        return GroupedOpenApi.builder().group("*").pathsToMatch("/**").build();
+    }
 }
--- lokakarya/src/main/java/co/id/ogya/lokakarya/services/DivisionServ.java
+++ lokakarya/src/main/java/co/id/ogya/lokakarya/services/DivisionServ.java
@@ -1,5 +1,16 @@
 package co.id.ogya.lokakarya.services;
 
-public interface DivisionRepo {
-    
+import co.id.ogya.lokakarya.dto.division.DivisionCreateDto;
+import co.id.ogya.lokakarya.dto.division.DivisionGetDto;
+import co.id.ogya.lokakarya.dto.division.DivisionUpdateDto;
+
+import java.util.List;
+
+public interface DivisionServ {
+    List<DivisionGetDto> getAll();
+    DivisionGetDto getById(String id);
+    DivisionCreateDto create(DivisionCreateDto divisionCreateDto);
+    DivisionUpdateDto update(DivisionUpdateDto divisionUpdateDto);
+    boolean delete(String id);
+
 }
