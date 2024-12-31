46d5405af265517e2204fa703148cede529c01f4
Bagas Yudhistira Fauzi <bagasyudhistira64@gmail.com>
2024-12-31 10:13:47 +0700

    Updated Sorch

--- src/main/java/co/id/ogya/lokakarya/repositories/impl/AssessmentSummaryRepoImpl.java
+++ src/main/java/co/id/ogya/lokakarya/repositories/impl/AssessmentSummaryRepoImpl.java
@@ -290,7 +290,7 @@ public class AssessmentSummaryRepoImpl implements AssessmentSummaryRepo {
     @Override
     public List<Map<String, Object>> sorchAssessmentSummaryGets(String keyword, String column, String order, int page, int pageSize, String divisionId, int assessmentYear) {
         int offset = (page - 1) * pageSize;
-        String sql = "SELECT ass.ID, USER_ID, au.FULL_NAME, YEAR, SCORE, STATUS, DIVISION_NAME, APPROVED_AT, ap.FULL_NAME AS APPROVER_NAME FROM tbl_assessment_summary ass LEFT JOIN tbl_app_user au ON ass.USER_ID = au.ID LEFT JOIN tbl_division dv ON au.DIVISION_ID = dv.ID LEFT JOIN tbl_app_user ap ON ass.APPROVED_BY = ap.ID WHERE LOWER(au.FULL_NAME) LIKE LOWER(CONCAT('%', COALESCE(?, ''), '%')) OR CAST(SCORE as VARCHAR(10)) LIKE LOWER(CONCAT('%', COALESCE(?, ''), '%')) OR LOWER(ap.FULL_NAME) LIKE LOWER(CONCAT('%', COALESCE(?, ''), '%'))";
+        String sql = "SELECT ass.ID, USER_ID, au.FULL_NAME, YEAR, SCORE, STATUS, DIVISION_NAME, APPROVED_AT, ap.FULL_NAME AS APPROVER_NAME FROM tbl_assessment_summary ass LEFT JOIN tbl_app_user au ON ass.USER_ID = au.ID LEFT JOIN tbl_division dv ON au.DIVISION_ID = dv.ID LEFT JOIN tbl_app_user ap ON ass.APPROVED_BY = ap.ID WHERE (LOWER(au.FULL_NAME) LIKE LOWER(CONCAT('%', COALESCE(?, ''), '%')) OR CAST(SCORE as VARCHAR(10)) LIKE LOWER(CONCAT('%', COALESCE(?, ''), '%')) OR LOWER(ap.FULL_NAME) LIKE LOWER(CONCAT('%', COALESCE(?, ''), '%')))";
 
         if (divisionId != null) {
             sql += " AND au.DIVISION_ID = ? AND YEAR = ?";
